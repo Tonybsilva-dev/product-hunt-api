72cc377cc58b7f8784e404346d4175cccc6809fe
Antonio Bernardino da Silva <antoniobsilvaprof@hotmail.com>
2020-12-06 22:12:32 -0300

    Middleware de logs adicionado

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const cors = require('cors')
 const requireDir = require('require-dir');
 
-
 //Iniciando o app
 const app = express()
 //habilitado o CORS
@@ -13,11 +12,22 @@ app.use(express.json())
 
 //Iniciando o bando de dados
 mongoose.connect('mongodb://localhost:27017/product-hunt', { useNewUrlParser: true })
+
+// Middleware de LOGS de rotas
+function logRequests(req, res, next){
+    const { method, url } = req;
+    const logLabel = `[${method.toUpperCase()}] ${url}`
+    console.log(logLabel);
+    console.time(logLabel);
+    next();
+    console.timeEnd(logLabel);
+  }
+  app.use(logRequests)
+
 //Requisitando os models
 requireDir('./src/models')
 
 
-
 app.use('/api', require('./src/routes'))
-app.listen(3001)
+app.listen(3001 || 3002)
 
--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,6 @@ const routes = express.Router();
 const ProductController = require('./controllers/ProductController')
 
 
-
 //Primeira Rota
 routes.get('/products', ProductController.index)
 routes.get('/products/:id', ProductController.show)
